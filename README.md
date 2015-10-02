f15ee7ba19ba62566ca30d7faa5c63f3da121e7b
Danny <mr_dark@darkweb.nl>
2015-10-02 18:42:34 +0200

    Initial commit
    Basic ASP.NET 4.6 MVC template + NuGet.Server package. We'll have to see
    if NuGet.Server package gives us enough freedom for what we want to
    accomplish.
    
    Tried to use the new APS.NET 5 template but wasn't able to get
    NuGet.Server package working. If we'd like to switch we'll probably have
    to write the server ourselves.

--- README.md
+++
@@ -1,2 +0,0 @@
-# YourGet
-For when you want to run your own Nuget server
---
+++ YourGet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YourGet.Startup))]
+namespace YourGet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
